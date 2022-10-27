2aaece5fd50d9640c1f9ea7755caf190ee2ff8df
Philip Vollmann <Philip.vollmann0408@gmail.com>
2022-10-27 11:07:12 +0200

    AscII art, when Board is full function

--- board.js
+++ board.js
@@ -25,55 +25,30 @@ module.exports = {
       },
   
 
-  getWinningPlayer: function (board) {
+  getWinningPlayer: function (symbol, board) {
     
-    return[
-
-      board[0][0]=== "X"  && board[0][1]=== "X" && board[0][2]=== "X",
-          
-        
-       board[1][0]=== "X" && board[1][1]=== "X" && board[1][2]=== "X",
-         
-        
-       board[2][0] === "X"&& board[2][1]=== "X" && board[2][2]=== "X",
-          
-        
-        board[0][0]=== "X" && board[1][0]=== "X" && board[2][0]=== "X",
-          
-        
-       board[0][1]=== "X" && board[1][1]=== "X" && board[2][1]=== "X",
-        
-       
-       board[0][2] === "X"&& board[1][2]=== "X" && board[2][2]=== "X",
-
-       board[0][0] === "X" && board[1][1]=== "X" && board[2][2]=== "X",
-        
-       board[2][0] === "X"&& board[1][1] === "X"&& board[0][2]=== "X",
-         
+   
     
-    
-      board[0][0] === "O" && board[0][1]=== "O" && board[0][2]=== "O",
+      return[
+        
+  
+    board[0][0] === symbol  && board[0][1] === symbol && board[0][2] === symbol,
+        
+    board[1][0] === symbol && board[1][1] === symbol && board[1][2] === symbol,
+       
+    board[2][0] === symbol&& board[2][1] === symbol && board[2][2] === symbol,
+        
+    board[0][0] === symbol && board[1][0] === symbol && board[2][0] === symbol,
+        
+    board[0][1] === symbol && board[1][1] === symbol && board[2][1] === symbol,
+      
+    board[0][2] === symbol&& board[1][2] === symbol && board[2][2] === symbol,
 
-      board[1][0] === "O"&& board[1][1]=== "O" && board[1][2]=== "O",
-          
-      board[2][0]=== "O" && board[2][1]=== "O" && board[2][2]=== "O",
-          
-        
-      board[0][0]=== "O" && board[1][0]=== "O" && board[2][0]=== "O",
-          
-      board[0][1]=== "O" && board[1][1]=== "O" && board[2][1]=== "O",
-        
+    board[0][0] === symbol && board[1][1] === symbol && board[2][2] === symbol,
+      
+    board[2][0] === symbol&& board[1][1] === symbol&& board[0][2] === symbol,
        
-       board[0][2]=== "O" && board[1][2]=== "O" && board[2][2]=== "O",
-        
-       
-       board[0][0]=== "O" && board[1][1]=== "O" && board[2][2]=== "O",
-        
-        
-       board[2][0] === "O"&& board[1][1]=== "O" && board[0][2]=== "O",
-          
-        
-      ].includes(true)
+    ].includes(true)
     /*
       Should return the player that wins based on the tic tac toe rules.
       If no player has won, than "None" is returned.
--- ticTacToe.js
+++ ticTacToe.js
@@ -1,7 +1,8 @@
-const {getMenuOption}= require("./menu"); // use it e.g. like menu.get_menu_option()
+const {getMenuOption,MenuAscIIArt}= require("./menu"); // use it e.g. like menu.get_menu_option()
 const {getEmptyBoard, 
       displayBoard,
       isBoardFull,
+      getWinningPlayer,
       } = require("./board");
 const coordinate = require("./coordinates");
 const prompt = require("prompt-sync")();
@@ -15,24 +16,60 @@ let playerSymbol = "X";
 
 
 function main() {
+    console.log('\x1b[36m%s\x1b[0m',MenuAscIIArt)
   let gameMode = getMenuOption();
   let gameBoard = getEmptyBoard();
   let isGameRunning = true;
+  let symbol = "X" || "O";
 
   while (isGameRunning) {
-    getMenuOption(gameMode);
     displayBoard(gameBoard);
     getPlayerMove(gameBoard);
 
-    /* TODO
+    if (isBoardFull(gameBoard) === true){
+        console.log(displayBoard(gameBoard));
+        
+        console.log("Thank you for playing \n"+
+                     "Do you want to play again \n"+
+                     "Enter 'yes' or 'no'.")
+        let userInput = prompt (": ")
 
+        while( userInput !== "yes".toLowerCase() && userInput !== "no".toLowerCase()){
+            console.log("Please only enter 'yes' or 'no'.")
+            return userInput = prompt (": ");
+        }
+        if (userInput === "yes".toLowerCase()){
+          return getMenuOption(gameMode);
+        }
+        if (userInput === "no".toLowerCase()){
+          process.exit()
+        }
+      }
+      if (getWinningPlayer(symbol, gameBoard) === true){
+        console.log(displayBoard(gameBoard))
+        console.log ("Congratulations "+ symbol+" Wins \n"+
+                     "Do you want to play again \n"+
+                     "Enter 'yes' or 'no'.");
+                     let userInput = prompt (": ")
+
+        while( userInput !== "yes".toLowerCase() && userInput !== "no".toLowerCase()){
+            console.log("Please only enter 'yes' or 'no'.")
+            return userInput = prompt (": ");
+      }
+      if (userInput === "yes".toLowerCase()){
+        return getMenuOption(gameMode);
+      }
+      if (userInput === "no".toLowerCase()){
+        process.exit()
+      }
+
+    /* TODO
         in each new iteration of the while loop the program should 
         alternate the value of `currentPlayer` from `X` to `O`
         */
     //let currentPlayer = "X";
 
     /* TODO
-
         based on the value of the variables `game_mode` and `currentPlayer` 
         the programm should should choose betwen the functions
         get_random_ai_coordinates or get_umbeatable_ai_coordinates or get_human_coordinates
@@ -42,13 +79,13 @@ function main() {
     //gameBoard[humanCoord[0]][humanCoord[1]] = currentPlayer;
 
     /* TODO 
-
         based on the values of `winning_player` and `its_a_tie` the program
         should either stop displaying a winning/tie message 
         OR continue the while loop
         */
     //let winningPlayer = board.getWinningPlayer(gameBoard);
     //let itsATie = board.isBoardFull(gameBoard);
-  
-
+  }
+}
+}
 main();
